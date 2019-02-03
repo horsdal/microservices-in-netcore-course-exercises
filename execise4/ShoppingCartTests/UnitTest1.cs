af22fc137459318c1d46a5dd4bbb87975b349075
Christian Horsdal <c.horsdal@gmail.com>
2019-02-03 21:37:32 +0100

    fix structure and turn tests into inegtation tests

--- execise4/ShoppingCartTests/UnitTest1.cs
+++ execise4/ShoppingCartTests/UnitTest1.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Marten;
 using Nancy;
 using Nancy.Testing;
 using ShoppingCart;
@@ -16,21 +18,30 @@ namespace ShoppingCartTests
         public ShoppingCart_Should()
         {
             userId = 123;
-            sut = new Browser(with => with.Module<Cart>(), defaults => defaults.Accept("application/json"));
+            sut = new Browser(with => 
+                {
+                    with.Module<Cart>();
+                    with.Dependency<IDocumentStore>(DocumentStore.For(x => 
+                    {
+                        x.Connection("host=localhost;database=shoppingcart;password=mysecretpassword;username=postgres");
+                    }));
+                },
+                defaults => defaults.Accept("application/json"));
         }
 
         [Fact]
         public async Task allow_for_adding_items()
         {
+            await sut.Delete($"/shoppingcart/{userId}");
             await AddProductsToCart("xbox", "playstation");
 
             var actual = await sut.Get($"/shoppingcart/{userId}");
-            var actualContent = actual.Body.DeserializeJson<Product[]>();
+            var actualContent = actual.Body.DeserializeJson<ShoppingCart.ShoppingCart>();
 
             Assert.Equal(HttpStatusCode.OK, actual.StatusCode);
-            Assert.Equal(2, actualContent.Length);
-            Assert.Contains("xbox", actualContent.Select(x => x.Name));
-            Assert.Contains("playstation", actualContent.Select(x => x.Name));
+            Assert.Equal(2, actualContent.Products.Count);
+            Assert.Contains("xbox", actualContent.Products.Select(x => x.Name));
+            Assert.Contains("playstation", actualContent.Products.Select(x => x.Name));
         }
 
         [Fact]
