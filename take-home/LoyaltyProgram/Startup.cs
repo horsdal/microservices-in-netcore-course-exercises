db707f32761952755fede329260d50e6f732ca60
Christian Horsdal <c.horsdal@gmail.com>
2020-11-02 15:31:42 +0100

    solve middleware

--- take-home/LoyaltyProgram/Startup.cs
+++ take-home/LoyaltyProgram/Startup.cs
@@ -1,9 +1,11 @@
 using System;
+using System.Threading.Tasks;
 using EasyNetQ;
 using LoyaltyProgram.SpecialOffers;
 using LoyaltyProgram.Users;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace LoyaltyProgram
@@ -23,8 +25,30 @@ namespace LoyaltyProgram
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseMiddleware<RedirectingMiddleware>();
             app.UseRouting();
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+        }
+    }
+
+    public class RedirectingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public RedirectingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public Task Invoke(HttpContext ctx)
+        {
+            if (ctx.Request.Path.Value.TrimEnd('/') == "/oldpath")
+            {
+                ctx.Response.Redirect("/newpath", permanent: true);
+                return Task.CompletedTask;
+            }
+
+            return _next(ctx);
         }
     }
 }
